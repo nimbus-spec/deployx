3e09b6ba59c84d385a2d37f41a81bf3643b19320
nimbus-spec <nimbus-spec@zvip.eu.org>
2026-04-03 14:42:40 +0800

    Use envsubst for template rendering to handle special chars

--- bin/render.sh
+++ bin/render.sh
@@ -12,26 +12,22 @@ Usage: $0 [options]
 Options:
     -t TEMPLATE    Template file path
     -v KEY=VALUE   Variable assignments (can be repeated)
-    -d DELIMITER   Delimiter (default: {{ }})
     -o OUTPUT      Output file (default: stdout)
     -h             Show this help
 
 Example:
     $0 -t template.tpl -v HOSTNAME=test -v PORT=22
-    $0 -t template.tpl -v KEY1=val1 -v KEY2=val2 -o output.txt
 EOF
 }
 
 TEMPLATE=""
-DELIMITER="{{ |}}"
 OUTPUT=""
 declare -a VARS
 
-while getopts "t:v:d:o:h" opt; do
+while getopts "t:v:o:h" opt; do
     case $opt in
         t) TEMPLATE="$OPTARG" ;;
         v) VARS+=("$OPTARG") ;;
-        d) DELIMITER="$OPTARG" ;;
         o) OUTPUT="$OPTARG" ;;
         h) usage; exit 0 ;;
         *) usage; exit 1 ;;
@@ -41,30 +37,24 @@ done
 [[ -z "$TEMPLATE" ]] && { echo "Error: -t TEMPLATE required"; exit 1; }
 [[ ! -f "$TEMPLATE" ]] && { echo "Error: Template not found: $TEMPLATE"; exit 1; }
 
-# Parse delimiter
-OLD_IFS="$IFS"
-IFS='|'
-read -r OPEN_DEL CLOSE_DEL <<< "$DELIMITER"
-IFS="$OLD_IFS"
-
-# Read template
-content=$(cat "$TEMPLATE")
-
-# Apply variables
+# Export variables for envsubst
 for var in "${VARS[@]}"; do
     key="${var%%=*}"
     value="${var#*=}"
-    
-    # Escape special characters for sed (use # as delimiter to avoid / conflicts)
-    value_escaped=$(echo "$value" | sed 's/[\/&]/\\&/g')
-    
-    # Replace {{ KEY }} with value (use # as sed delimiter)
-    content=$(echo "$content" | sed "s#${OPEN_DEL}[[:space:]]*${key}[[:space:]]*${CLOSE_DEL}#${value_escaped}#g")
+    export "$key"="$value"
 done
+
+# Use envsubst to replace ${VAR} style placeholders
+# Convert {{ VAR }} to ${VAR} for envsubst
+temp_file=$(mktemp)
+sed 's/{{ *\([A-Za-z_][A-Za-z0-9_]*\) *}}/${\1}/g' "$TEMPLATE" > "$temp_file"
+
+result=$(envsubst < "$temp_file")
+rm -f "$temp_file"
 
 # Output
 if [[ -n "$OUTPUT" ]]; then
-    echo "$content" > "$OUTPUT"
+    echo "$result" > "$OUTPUT"
 else
-    echo "$content"
+    echo "$result"
 fi
